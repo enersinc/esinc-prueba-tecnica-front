e77cebc8460b73b960077babfa209146f11a7a6b
JoseBruges <josedavid.bruges@gmail.com>
2024-07-08 17:24:02 -0500

    issue 1, 2, 3 y 4

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect,  useState } from 'react'
 import { fetchData } from '../../services/fetchData';
-import { Button, Col, Row } from 'antd';
+import { Button, Col, Row, Alert, Pagination } from 'antd';
 import PokemonCard from '../PokemonCard/PokemonCard';
 import SearchPokemon from './SearchPokemon';
 import PokemonDetail from './PokemonDetail';
@@ -8,10 +8,14 @@ import PokemonDetail from './PokemonDetail';
 export default function PokemonList() {
 
   const [pokemons, setPokemons] = useState([])
+  const [pokemonsBase, setPokemonsBase] = useState([])
   const [currentPage, setCurrentPage] = useState(0)
   const [searchList, setSearchList] = useState([])
   const [openDetail, setOpenDetail] = useState(false)
   const [currentPokemon, setCurrentPokemon] = useState({})
+  const [totalPages, setTotalPages] = useState(0)
+
+  const [alertError, setAlertError] = useState(false)
 
   const getPokemonIdByUrl = (url) => {
     const segments = url.split("/");
@@ -23,12 +27,11 @@ export default function PokemonList() {
   const getPokemonInfo = async (list) => {
 
     try {
-
-      const pokemonInfo = list.map(async (pokemon) => {
+      const pokemonInfo = []
+      for(const pokemon of list){
         const response = await fetchData({ endpoint: `pokemon/${getPokemonIdByUrl(pokemon.url)}` })
-        return response
-      })
-
+        pokemonInfo.push(response)
+      }
       const pokemonList = list.map((pokemon, index) => {
         return {
           name: pokemon.name,
@@ -40,33 +43,41 @@ export default function PokemonList() {
       return pokemonList
     } catch (error) {
       console.error('Error fetching Pokemon info:', error);
+      setAlertError(true);
     }
   }
 
   const getData = async () => {
-
     try {
-      const { results: allPokemons } = await fetchData({ endpoint: `pokemon/?limit=20&offset=${currentPage * 20}` })
+      const { results: allPokemons, count } = await fetchData({ endpoint: `pokemon/?limit=20&offset=${currentPage * 20}` })
+      setTotalPages(count)
       const pokemonList = await getPokemonInfo(allPokemons)
-      currentPage > 0 ? setPokemons(prevState => [prevState, pokemonList]) : setPokemons(pokemonList)
+      setPokemons(pokemonList)
+      setPokemonsBase(pokemonList)
     } catch (error) {
       console.error('Error fetching Pokemon info:', error);
+      setAlertError(true);
     }
   }
 
   const handleSearchPokemon = async (value) => {
+    console.debug('\n\n\n\t handleSearchPokemon: ', value)
 
     try {
       if (value) {
         const { results: allPokemons } = await fetchData({ endpoint: 'pokemon/?limit=10000&offset=0' });
         const filterPokemons = allPokemons.filter((pokemon) => pokemon.name.includes(value));
+        const pokemonsNoFilter = allPokemons.filter((pokemon) => !pokemon.name.includes(value));
+        setPokemons([...pokemonsNoFilter])
         const pokemonList = await getPokemonInfo(filterPokemons);
         setSearchList(pokemonList);
       } else {
         setSearchList([]);
+        setPokemons([...pokemonsBase])
       }
     } catch (error) {
       console.error('Error searching Pokemon:', error);
+      setAlertError(true);
     }
   }
 
@@ -74,10 +85,9 @@ export default function PokemonList() {
     getData()
   }, [currentPage]);
 
-  const loadPokemons = () => {
-    setCurrentPage(prevState => prevState + 1)
-
-  }
+  const loadPokemons = (page) => {
+    setCurrentPage(page - 1)
+  };
 
   const onCardClick = async (name) => {
 
@@ -95,11 +105,15 @@ export default function PokemonList() {
     setOpenDetail(false)
   }
 
+
   return (
     <Row gutter={[0, 24]}>
       <Row style={{ width: "100%" }} align={"end"}>
         <SearchPokemon onSearch={handleSearchPokemon} />
       </Row>
+
+      {alertError && <Alert message="Oups! No se puede visualizar la lista de Pokemones." type="error" />}
+
       <Row gutter={[12, 12]}>
         {searchList.length > 0 &&
           searchList.map((pokemon) => (
@@ -118,9 +132,9 @@ export default function PokemonList() {
         }
         
       </Row>
-      <Row align={"center"} style={{ width: "100%" }}>
-        <Button type='primary' onClick={loadPokemons}>Cargar mas...</Button>
-      </Row>
+      {!alertError && (searchList.length > 0 || pokemons.length > 0) && <Row align={"center"} style={{ width: "100%" }}>
+        <Pagination defaultCurrent={1} current={currentPage + 1} onChange={loadPokemons} total={totalPages} />
+      </Row>}
 
       <PokemonDetail {...currentPokemon} openModal={openDetail} handleOpenModal={handleCloseDetail} />
     </Row>
