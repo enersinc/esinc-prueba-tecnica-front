f3dfaa596f661475e4c170861ae2e93665663518
anderson rangel <andergroundmsg@gmail.com>
2024-07-09 09:32:23 -0500

    solved async issue

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,  useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { fetchData } from '../../services/fetchData';
 import { Button, Col, Row } from 'antd';
 import PokemonCard from '../PokemonCard/PokemonCard';
@@ -6,56 +6,54 @@ import SearchPokemon from './SearchPokemon';
 import PokemonDetail from './PokemonDetail';
 
 export default function PokemonList() {
-
-  const [pokemons, setPokemons] = useState([])
-  const [currentPage, setCurrentPage] = useState(0)
-  const [searchList, setSearchList] = useState([])
-  const [openDetail, setOpenDetail] = useState(false)
-  const [currentPokemon, setCurrentPokemon] = useState({})
+  const [pokemons, setPokemons] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [searchList, setSearchList] = useState([]);
+  const [openDetail, setOpenDetail] = useState(false);
+  const [currentPokemon, setCurrentPokemon] = useState({});
 
   const getPokemonIdByUrl = (url) => {
-    const segments = url.split("/");
-    const pokemonId = segments[segments.length - 2]
-    return pokemonId
-
-  }
+    const segments = url.split('/');
+    const pokemonId = segments[segments.length - 2];
+    return pokemonId;
+  };
 
   const getPokemonInfo = async (list) => {
-
     try {
+      const pokemonPromises = list.map(async (pokemon) => {
+        const response = await fetchData({ endpoint: `pokemon/${getPokemonIdByUrl(pokemon.url)}` });
+        return response;
+      });
 
-      const pokemonInfo = list.map(async (pokemon) => {
-        const response = await fetchData({ endpoint: `pokemon/${getPokemonIdByUrl(pokemon.url)}` })
-        return response
-      })
+      const pokemonInfo = await Promise.all(pokemonPromises);
 
       const pokemonList = list.map((pokemon, index) => {
         return {
           name: pokemon.name,
           pokemonImg: pokemonInfo[index].sprites.other.dream_world.front_default,
-          types: pokemonInfo[index].types
-        }
+          types: pokemonInfo[index].types,
+        };
       });
 
-      return pokemonList
+      return pokemonList;
     } catch (error) {
       console.error('Error fetching Pokemon info:', error);
     }
-  }
+  };
 
   const getData = async () => {
-
     try {
-      const { results: allPokemons } = await fetchData({ endpoint: `pokemon/?limit=20&offset=${currentPage * 20}` })
-      const pokemonList = await getPokemonInfo(allPokemons)
-      currentPage > 0 ? setPokemons(prevState => [prevState, pokemonList]) : setPokemons(pokemonList)
+      const { results: allPokemons } = await fetchData({
+        endpoint: `pokemon/?limit=20&offset=${currentPage * 20}`,
+      });
+      const pokemonList = await getPokemonInfo(allPokemons);
+      setPokemons((prevState) => (currentPage > 0 ? [...prevState, ...pokemonList] : pokemonList));
     } catch (error) {
       console.error('Error fetching Pokemon info:', error);
     }
-  }
+  };
 
   const handleSearchPokemon = async (value) => {
-
     try {
       if (value) {
         const { results: allPokemons } = await fetchData({ endpoint: 'pokemon/?limit=10000&offset=0' });
@@ -68,61 +66,57 @@ export default function PokemonList() {
     } catch (error) {
       console.error('Error searching Pokemon:', error);
     }
-  }
+  };
 
   useEffect(() => {
-    getData()
+    getData();
   }, [currentPage]);
 
   const loadPokemons = () => {
-    setCurrentPage(prevState => prevState + 1)
-
-  }
+    setCurrentPage((prevState) => prevState + 1);
+  };
 
   const onCardClick = async (name) => {
-
-    const pokemon = await fetchData({ endpoint: `pokemon/${name}` })
-
+    const pokemon = await fetchData({ endpoint: `pokemon/${name}` });
     setCurrentPokemon({
       name: name,
       pokemonImg: pokemon.sprites.other.dream_world.front_default,
-      stats: pokemon.stats
-    })
-
-  }
+      stats: pokemon.stats,
+    });
+    setOpenDetail(true);
+  };
 
   const handleCloseDetail = () => {
-    setOpenDetail(false)
-  }
+    setOpenDetail(false);
+  };
 
   return (
     <Row gutter={[0, 24]}>
-      <Row style={{ width: "100%" }} align={"end"}>
+      <Row style={{ width: '100%' }} align={'end'}>
         <SearchPokemon onSearch={handleSearchPokemon} />
       </Row>
       <Row gutter={[12, 12]}>
         {searchList.length > 0 &&
           searchList.map((pokemon) => (
-            <Col span={6} >
+            <Col span={6} key={pokemon.name}>
               <PokemonCard {...pokemon} onClick={onCardClick} />
             </Col>
-          ))
-        }
+          ))}
 
         {pokemons.length > 0 &&
           pokemons.map((pokemon) => (
-            <Col span={6} >
+            <Col span={6} key={pokemon.name}>
               <PokemonCard {...pokemon} onClick={onCardClick} />
             </Col>
-          ))
-        }
-        
+          ))}
       </Row>
-      <Row align={"center"} style={{ width: "100%" }}>
-        <Button type='primary' onClick={loadPokemons}>Cargar mas...</Button>
+      <Row align={'center'} style={{ width: '100%' }}>
+        <Button type="primary" onClick={loadPokemons}>
+          Cargar más...
+        </Button>
       </Row>
 
       <PokemonDetail {...currentPokemon} openModal={openDetail} handleOpenModal={handleCloseDetail} />
     </Row>
-  )
+  );
 }
